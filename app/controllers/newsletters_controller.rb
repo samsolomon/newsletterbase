e59d14d692bc0bf45b45a28ef766089a891eda2e
samsolomon <samuelrobertsolomon@gmail.com>
2015-12-06 20:37:35 -0500

    updated posts controller

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -1,4 +1,43 @@
 class NewslettersController < ApplicationController
+  before_action :find_newsletter, only: [:show, :edit, :update, :destroy]
+
   def index
+    @newsletters = Newsletter.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @newsletter = Newsletter.new
+  end
+
+  def create
+    @newsletter = Newsletter.new(newsletter_params)
+
+    if @newsletter.save
+      redirect_to @newsletter
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_newsleter
+    @newsletter = Newsletter.find(params[:id])
+  end
+
+  def newsletter_params
+    params.require(:newsletter).permit(:name, :author, :subscribe_link, :description)
   end
 end
